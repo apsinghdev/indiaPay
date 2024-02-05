4236e69ef4a3685de5a11c640cd79de2656bd25c
apsinghdev <ajeetpratap517@gmail.com>
2024-02-05 23:37:16 +0530

    add cors mw

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
-import Express from "express";
+import express from "express";
 import router from "./routes/index";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use("/api/v1", router);
 
