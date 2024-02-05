01ccc7a9054312ee5861bfd096d14d8dc16a04ba
apsinghdev <ajeetpratap517@gmail.com>
2024-02-05 23:48:58 +0530

    add jwt and listen app on port 4000

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import router from "./routes/index";
 import cors from "cors";
 
+const PORT = 4000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", router);
+app.listen(PORT);
 
 
 
