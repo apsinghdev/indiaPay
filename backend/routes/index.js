743763adba258107dd96cd0c81fd2363de7b7144
apsinghdev <ajeetpratap517@gmail.com>
2024-02-05 23:26:33 +0530

    add user router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 import Express from "express";
+import userRouter from "./user";
 
 const router = Express.Router();
+
+router.use("/api/v1/user", userRouter);
 
 module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+module.exports = userRouter;
