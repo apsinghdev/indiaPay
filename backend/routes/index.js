bd46574730937db228ca518e2fd6d70d58d4591d
apsinghdev <ajeetpratap517@gmail.com>
2024-02-12 19:58:53 +0530

    add route for account

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 import Express from "express";
 import userRouter from "./user";
+import accountRouter from "./account";
 
 const router = Express.Router();
 
 router.use("/api/v1/user", userRouter);
+router.use("/api/v1/account", accountRouter);
 
 module.exports = router;
