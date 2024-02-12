e2711a241aaac693cd7d59774d7ac736c808b27b
apsinghdev <ajeetpratap517@gmail.com>
2024-02-12 20:20:55 +0530

    implement balance route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,22 @@
 import express from "express";
+import Account from "../models/accounts.model"
+import authMiddleware from "../middleware";
 
 const router = express.Router();
+
+router.get('/balance', authMiddleware, showBalance);
+
+// function that gets the balance of an user
+
+async function showBalance(req, res){
+    try{
+        const account = await Account.findOne({userId: req.userId});
+        res.send(200).json({balance: account.balance});
+
+    } catch(error){
+        res.send(403).json({error: error});
+    }
+    
+}
 
 module.exports = router;
