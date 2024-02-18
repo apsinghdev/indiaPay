39f3caac30231b25bffb405407c2c29482fc8459
apsinghdev <ajeetpratap517@gmail.com>
2024-02-18 23:47:40 +0530

    add tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Singnin from "./components/Signin";
 import Signup from "./components/Signup";
-import Dashboard from "./components/Dashboard";
-import SendMoney from "./components/SendMoney";
+// import Singnin from "./components/Signin";
+// import Dashboard from "./components/Dashboard";
+// import SendMoney from "./components/SendMoney";
 
 function App() {
   return (
@@ -10,9 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/signin" element={<Singnin />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/send" element={<SendMoney />}></Route>
+          {/* <Route path="/signin" element={<Singnin />}></Route> */}
+          {/* <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
