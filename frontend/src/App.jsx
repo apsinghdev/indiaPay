80c078c15e78136c8b3a633083857359467aba40
apsinghdev <ajeetpratap517@gmail.com>
2024-02-18 23:12:06 +0530

    add client side routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Singnin from "./components/Signin";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import SendMoney from "./components/SendMoney";
 
 function App() {
-
   return (
     <div>
-        Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Singnin />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
